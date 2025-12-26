bafe2a9d80be7959683d7efb75bb8a67aec1ee7f
Ulrich Lehner <me@ulrichlehner.com>
2025-12-26 12:40:13 +0100

    fix: create venv dir during devcontainer start

--- .devcontainer/setup-venv.sh
+++ .devcontainer/setup-venv.sh
@@ -3,8 +3,13 @@ set -euo pipefail
 
 VENV_DIR="/opt/venv"
 
-if [ -d "$VENV_DIR" ] && [ "$(id -u)" -ne 0 ]; then
-  if ! [ -w "$VENV_DIR" ]; then
+if [ ! -d "$VENV_DIR" ]; then
+  if [ "$(id -u)" -ne 0 ]; then
+    sudo mkdir -p "$VENV_DIR"
     sudo chown -R "$(id -u)":"$(id -g)" "$VENV_DIR"
+  else
+    mkdir -p "$VENV_DIR"
   fi
+elif [ "$(id -u)" -ne 0 ] && ! [ -w "$VENV_DIR" ]; then
+  sudo chown -R "$(id -u)":"$(id -g)" "$VENV_DIR"
 fi
