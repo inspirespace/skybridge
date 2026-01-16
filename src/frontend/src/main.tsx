55c8679a709c6cec3290ee5636ec2dbac2db411c
Ulrich Lehner <me@ulrichlehner.com>
2026-01-16 23:38:56 +0100

    fix: patch emulator requests before app load

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import { patchFirebaseEmulatorRequests } from "./lib/firebase-emulator";
 
 patchFirebaseEmulatorRequests();
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+import("./App.tsx").then(({ default: App }) => {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+});
